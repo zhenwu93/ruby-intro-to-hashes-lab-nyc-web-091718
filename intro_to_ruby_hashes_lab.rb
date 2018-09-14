2d20646960ce3e76bf4e201d22e731833c7ad61e
Zhen Wei Wu <zhenwu93@gmail.com>
2018-09-14 01:00:42 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -15,8 +15,8 @@ end
 
 def monopoly_with_second_tier
   monopoly = {:railroads => {
-    :pieces => 4
-    :names => {}
+    :pieces => 4,
+    :names => {},
     :rent_in_dollars => {}
     }
   }
